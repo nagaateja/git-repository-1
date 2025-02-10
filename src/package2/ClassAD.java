0b85828fd04fa5527d538e816fb334183d372380
Nagateja <nagani@leftbrainpath.com>
2025-02-10 19:32:59 +0530

    This is Updated ClassAD in project-1

--- src/package2/ClassAD.java
+++ src/package2/ClassAD.java
@@ -3,5 +3,6 @@ package package2;
 public class ClassAD {
     public static void main(String[] args) {
         System.out.println("This is ClassAD");
+        System.out.println("This is Updated ClassAD");
     }
 }
